bd4f6603b9fc0ca62b1f2a33d2be88be92f70412
Joshua Okorie <joshuaokorie008@gmail.com>
2024-01-21 06:20:20 +0100

    fetch data from tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
+
+import Results from '@/components/Results'
 import React from 'react'
 
-const page = () => {
+const API_KEY = process.env.API_KEY
+
+
+
+const Home = async ({searchParams}) => {
+  const genre=searchParams.genre||'fetchTrending'
+  const response = await fetch(`https://api.themoviedb.org/3${genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`)
+  const data = await response.json()
+  if (!response.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  const results = data.results;
+  console.log(results);
   return (
-    <div>page</div>
+    <div><Results results={results} /></div>
   )
 }
 
-export default page
+export default Home
