e980bfc4af91dd530693fd98edef701f74653ff4
Joshua Okorie <joshuaokorie008@gmail.com>
2024-01-30 00:33:50 +0100

    add loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ const API_KEY = process.env.API_KEY
 
 const Home = async ({searchParams}) => {
   const genre=searchParams.genre||'fetchTrending'
-  const response = await fetch(`https://api.themoviedb.org/3${genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`)
+  const response = await fetch(`https://api.themoviedb.org/3${genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`,{next:{revalidate:10000}})
   const data = await response.json()
   if (!response.ok) {
     throw new Error('Failed to fetch data')
